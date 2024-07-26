4c8ac8f45ea3fd018ac1164fc43c04d7ea538fa5
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-07-26 16:42:15 -0400

    explicit pretender import

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 /* eslint-env node */
 'use strict';
-const fs = require('fs');
-const Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: require('./package').name,
   isDevelopingAddon: function () {
     return false;
-  },
-
-  treeForApp: function (appTree) {
-    if (!this.includeFactoryGuyFiles) {
-      return appTree;
-    }
-
-    const trees = [appTree];
-
-    try {
-      if (fs.statSync('tests/factories').isDirectory()) {
-        const factoriesTree = new Funnel('tests/factories', {
-          destDir: 'tests/factories',
-        });
-        trees.push(factoriesTree);
-      }
-    } catch (err) {
-      // do nothing;
-    }
-
-    const MergeTrees = require('broccoli-merge-trees');
-
-    return MergeTrees(trees);
   },
 };
