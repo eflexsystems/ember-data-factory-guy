75bddc47de24f1ecec3fa896ff51594806c75dcb
Jacob Jewell <jacobjewell@eflexsystems.com>
2024-07-26 16:30:50 -0400

    cleanup

--- index.js
+++ index.js
@@ -118,24 +118,5 @@ module.exports = {
     }
 
     this.includeFactoryGuyFiles = settings.enabled;
-
-    // Have to be carefull not to exclude factory guy from addon tree
-    // in development or test env
-    const trees = /test|development/.test(app.env) ? 'app' : 'app|addon';
-    this.treeExcludeRegex = new RegExp(trees);
-  },
-
-  treeFor: function (name) {
-    // Not sure why this is necessary, but this stops the factory guy files
-    // from being added to app tree. Would have thought that this would have
-    // happened in treeForApp above, but not the case
-    if (
-      !this.includeFactoryGuyFiles &&
-      this.treeExcludeRegex &&
-      this.treeExcludeRegex.test(name)
-    ) {
-      return;
-    }
-    return this._super.treeFor.apply(this, arguments);
   },
 };
