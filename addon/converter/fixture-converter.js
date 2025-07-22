892927a94c4a39bae75150705a274e3c3f46b07e
Rachel Glomski <rrglomsk@users.noreply.github.com>
2025-07-22 15:13:39 -0400

    update (#4)
    * update
    
    * oop
    
    * lint?

--- addon/converter/fixture-converter.js
+++ addon/converter/fixture-converter.js
@@ -263,11 +263,9 @@ export default class FixtureConverter {
       if (macroCondition(dependencySatisfies('ember-data', '>=5.3.0'))) {
         relationshipsDefinition = this.store.schema.fields({ type: modelName });
       } else {
-        relationshipsDefinition = this.store
-          .getSchemaDefinitionService()
-          .relationshipsDefinitionFor({
-            type: modelName,
-          });
+        relationshipsDefinition = this.store.schema.relationshipsDefinitionFor({
+          type: modelName,
+        });
       }
 
       const relationship =
